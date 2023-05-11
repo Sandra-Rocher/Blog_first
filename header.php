706ac81124a8771cb46ada8b62bf52673a105cbe
Sandra-Rocher <sandra.rocher@hotmail.fr>
2023-05-11 22:16:02 +0200

    logo navigateur

--- header.php
+++ header.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- petit logo perso sur la page navigateur -->
+    <link rel="shortcut icon" type="image/x-icon" href="public/photoprofil.ico" />
     <!-- lien css -->
     <link href="public/stylesheet.css" rel="stylesheet">
     <!-- lien bootstrap -->
